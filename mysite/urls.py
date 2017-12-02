8f9321b1ee1cfc3b95baca880b0390e3ffa0d50e
Isabella <isabella.spinelli@gmail.com>
2017-12-02 16:43:57 +0100

    include blog urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)), 
+    url(r'', include('blog.urls')),
 ]
